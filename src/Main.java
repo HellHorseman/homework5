c92b59fd62570f12b2f59fcdff2ac256fbf2aafe
HellHorseman <duke.sheffer@yandex.ru>
2023-02-15 19:49:17 +0400

    add task 1

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,37 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+
     }
 
-    public static void task1 () {
+    public static void task1() {
         System.out.println("Задача 1");
+        int clientOs = 0;
+        if (clientOs == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOs == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
 
     }
 
-    public static void task2 () {
-        System.out.println("Задача 2");
+
+    public static void task3() {
+        System.out.println("Задача 3");
+
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
 
     }
 }
