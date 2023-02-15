5725ccbfcac0b0277d5898bef895eca11f89e303
HellHorseman <duke.sheffer@yandex.ru>
2023-02-15 20:29:54 +0400

    add task 2

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,19 @@ public class Main {
 
     public static void task2() {
         System.out.println("Задача 2");
-
+        int clientOs = 0;
+        int clientDeviceYear = 2020;
+        boolean old = (clientDeviceYear <= 2015);
+        if (clientOs == 0 && old) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOs == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        if (clientOs == 1 && old) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOs == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
     }
 
 
