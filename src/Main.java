8885c158cdbb00904fb056cd74e341daae6c538d
HellHorseman <duke.sheffer@yandex.ru>
2023-02-15 21:42:23 +0400

    add task 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
 
     }
 
@@ -14,7 +15,6 @@ public class Main {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
-
     public static void task2() {
         System.out.println("Задача 2");
         int clientOs = 0;
@@ -31,11 +31,14 @@ public class Main {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
-
-
     public static void task3() {
         System.out.println("Задача 3");
-
+        int year = 2021;
+        if ((year % 4 == 0) || ((year % 400 == 0) && (year % 100 != 0))) {
+            System.out.println(year + " является високосным");
+        } else {
+            System.out.println(year + " не является високосным");
+        }
     }
 
     public static void task4() {
