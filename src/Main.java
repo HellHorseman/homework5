defaedf69dddcb6758713006cfe011338b65c9e2
HellHorseman <duke.sheffer@yandex.ru>
2023-02-15 22:43:24 +0400

    add task 5

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
 
     }
 
@@ -16,6 +17,7 @@ public class Main {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
+
     public static void task2() {
         System.out.println("Задача 2");
         int clientOs = 0;
@@ -32,6 +34,7 @@ public class Main {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
+
     public static void task3() {
         System.out.println("Задача 3");
         int year = 2021;
@@ -41,26 +44,51 @@ public class Main {
             System.out.println(year + " не является високосным");
         }
     }
+
     public static void task4() {
         System.out.println("Задача 4");
-    int deliveryDistance = 95;
-    int days = 1;
-    if (deliveryDistance <= 20) {
-        days = 1;
-        System.out.println("Потребуется дней: " + days);
-    } else if (deliveryDistance > 20 && deliveryDistance <=60) {
-        days = days + 1;
-        System.out.println("Потребуется дней: " + days);
-    } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
-        days = days + 2;
-        System.out.println("Потребуется дней: " + days);
-    } else {
-        System.out.println("Доставки нет");
-    }
+        int deliveryDistance = 95;
+        int days = 1;
+        if (deliveryDistance <= 20) {
+            days = 1;
+            System.out.println("Потребуется дней: " + days);
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            days = days + 1;
+            System.out.println("Потребуется дней: " + days);
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            days = days + 2;
+            System.out.println("Потребуется дней: " + days);
+        } else {
+            System.out.println("Доставки нет");
+        }
     }
 
     public static void task5() {
         System.out.println("Задача 5");
-
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
+        }
     }
 }
